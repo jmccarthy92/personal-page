661f8308423b3af8af2b233de07b4ab304fd073c
Microsoft Learn Student <learn@contoso.com>
2024-01-20 23:20:37 -0500

    Upate font size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Typewriter from "typewriter-effect";
 
 function Header() {
   return (
-    <div style={{ fontSize: "5rem" }}>
+    <div style={{ fontSize: "4rem" }}>
       <Typewriter
         options={{
           strings: ["Hi, I'm James.", "I'm a Software Engineer."],
